9753cbcd60f1765a9f66e2df2c322be580b95943
Щербаков Захар1 <Sherbakov1@example.com>
2024-01-11 19:03:45 +0300

    begin exams and attandance

--- RecordBook/RecordBook/obj/Debug/net7.0/RecordBook.AssemblyInfo.cs
+++ RecordBook/RecordBook/obj/Debug/net7.0/RecordBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6524eef316bdaf158ff8cb181a55642446f4eb07")]
 [assembly: System.Reflection.AssemblyProductAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
