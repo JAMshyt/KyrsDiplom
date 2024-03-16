417dff74ffddbef7cb6440c96bc11dd9f6f13dfb
Щербаков Захар1 <Sherbakov1@example.com>
2024-03-16 15:28:32 +0300

    attendance change checkbox

--- RecordBook/RecordBook/obj/Debug/net7.0/RecordBook.AssemblyInfo.cs
+++ RecordBook/RecordBook/obj/Debug/net7.0/RecordBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea2f5aeb2127b67f14636df95bf39cc9cecc143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22cb257984eab770ca4c41872e0e1c69a43baa74")]
 [assembly: System.Reflection.AssemblyProductAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
