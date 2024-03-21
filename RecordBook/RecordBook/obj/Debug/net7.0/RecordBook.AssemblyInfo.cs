7d543b87e272e1cde1f61b89e35af760d4046527
Щербаков Захар1 <Sherbakov1@example.com>
2024-03-21 17:21:57 +0300

    return all commits

--- RecordBook/RecordBook/obj/Debug/net7.0/RecordBook.AssemblyInfo.cs
+++ RecordBook/RecordBook/obj/Debug/net7.0/RecordBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e95ffa872e13864c8c8f6cbc55e9710cfa0691")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71edc58eaca3bafe207f275c82a8985394d2e8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("recordBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
